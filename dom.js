3a011117cbe74ddfe8f580e456257319f395f536
Hubert Lin <linhub15@gmail.com>
2018-09-17 16:25:12 -0600

    works

---
+++ dom.js
@@ -0,0 +1,15 @@
+function el(tag, attrs, ...children) {
+  let element = tag ? document.createElement(tag) : null;
+  let keys = attrs && element ? Object.keys(attrs) : null;
+  let childNodes = children && element ? children : null;
+  for (let i in keys) {
+    element.setAttribute(keys[i], attrs[keys[i]]);
+  }
+  for (let node of childNodes) {
+    element.appendChild(
+      node instanceof HTMLElement ? node : document.createTextNode(node));
+  }
+  return element;
+}
+
+export {el};
---
+++ main.js
@@ -0,0 +1,26 @@
+import { el } from './dom.js'
+
+const countBusinessByCategory = 'https://data.edmonton.ca/resource/3trf-izgx.json?$select=business_category,count(*)&$group=business_category';
+
+fetch (countBusinessByCategory)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(categories) {
+        displayCategories(categories);
+    });
+
+/**
+ * Given a list of categories and their counts display them as cards
+ */
+
+ function displayCategories(categoryCounts) {
+    let categoryCards = el('div',{class:'card-deck'}); 
+    for (let c of categoryCounts) {
+        let card = el('div',{class:'card'}, 
+            el('p',{"class":"count"}, c.count_1),
+            el('div',{"class":"category"}, c.business_category));
+        categoryCards.appendChild(card);
+    }
+    document.querySelector('#byCategory').appendChild(categoryCards);
+ }
