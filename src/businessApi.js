22f430bc2ab40b83f9cb2c1d9a5c1d71fe4638c0
Hubert Lin <linhub15@gmail.com>
2018-09-19 15:02:55 -0600

    implement data access api

---
+++ src/businessApi.js
@@ -0,0 +1,24 @@
+export class businessApi {
+  constructor() {
+    this.apiEndpoint = 'https://data.edmonton.ca/resource/3trf-izgx.json' + '?';
+  }
+
+  async single(externalId) {
+    let apiQuery = this.apiEndpoint + `externalid=${externalId}`;
+    return await fetch(apiQuery).then(res => { return res.json();})
+  }
+
+  async listCategories() {
+    let apiQuery = this.apiEndpoint + `$select=business_category,count(*)&$group=business_category`;
+    return await fetch(apiQuery).then(res => { return res.json();})
+  }
+  async businessesByCategory(category) {
+    let apiQuery = this.apiEndpoint + `business_category=${category}`;
+    return await fetch(apiQuery).then(res => { return res.json();})
+  }
+  async businessesByName(tradeName) {
+    let apiQuery = this.apiEndpoint + `$where=trade_name like '${tradeName}%25'`;
+    return await fetch(apiQuery).then(res => { return res.json();})
+  }
+}
+
--- src/main.js
+++ src/main.js
@@ -1,26 +1,24 @@
 import { el } from './dom.js'
+import { businessApi } from './businessApi.js'
 
 const countBusinessByCategory = 'https://data.edmonton.ca/resource/3trf-izgx.json?$select=business_category,count(*)&$group=business_category';
 
-fetch (countBusinessByCategory)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(categories) {
-        displayCategories(categories);
-    });
+const data = new businessApi();
 
-/**
- * Given a list of categories and their counts display them as cards
- */
 
- function displayCategories(categoryCounts) {
-    let categoryCards = el('div',{class:'card-deck'}); 
-    for (let c of categoryCounts) {
-        let card = el('div',{class:'card'}, 
-            el('p',{"class":"count"}, c.count_1),
-            el('div',{"class":"category"}, c.business_category));
-        categoryCards.appendChild(card);
-    }
-    document.querySelector('#byCategory').appendChild(categoryCards);
- }
+
+data.single(11465).then(val => console.log(val));
+data.listCategories().then(val => displayCategories(val));
+data.businessesByCategory('Public Market Organizer').then(val => console.log(val));
+data.businessesByName('ON THE SPOT POP UP').then(val => {console.log(val)});
+
+function displayCategories(categoryCounts) {
+let categoryCards = el('div',{class:'card-deck'}); 
+for (let c of categoryCounts) {
+    let card = el('div',{class:'card'}, 
+        el('p',{"class":"count"}, c.count_1),
+        el('div',{"class":"category"}, c.business_category));
+    categoryCards.appendChild(card);
+}
+document.querySelector('#search').appendChild(categoryCards);
+}
