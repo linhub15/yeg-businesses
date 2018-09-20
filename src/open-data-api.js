82dd137788fe4f9b9f7136e06268d36d3efc8f5c
Hubert Lin - Macbook <linhub15@gmail.com>
2018-09-19 20:41:15 -0600

    format col max

--- src/open-data-api.js
+++ src/open-data-api.js
@@ -3,19 +3,23 @@ export class Business {
     this.apiEndpoint = 'https://data.edmonton.ca/resource/3trf-izgx.json' + '?';
   }
   async single(externalId) {
-    let apiQuery = this.apiEndpoint + `externalid=${externalId}`;
+    let apiQuery = 
+      this.apiEndpoint + `externalid=${externalId}`;
     return await fetch(apiQuery).then(res => { return res.json();})
   }
   async listCategories() {
-    let apiQuery = this.apiEndpoint + `$select=business_category,count(*)&$group=business_category`;
+    let apiQuery = 
+      this.apiEndpoint + '$select=business_category,count(*)&$group=business_category';
     return await fetch(apiQuery).then(res => { return res.json();})
   }
   async businessesByCategory(category) {
-    let apiQuery = this.apiEndpoint + `business_category=${category}`;
+    let apiQuery = 
+      this.apiEndpoint + `business_category=${category}`;
     return await fetch(apiQuery).then(res => { return res.json();})
   }
   async businessesByName(tradeName) {
-    let apiQuery = this.apiEndpoint + `$where=trade_name like '${tradeName}%25'`;
+    let apiQuery = 
+      this.apiEndpoint + `$where=trade_name like '${tradeName}%25'`;
     return await fetch(apiQuery).then(res => { return res.json();})
   }
 }
