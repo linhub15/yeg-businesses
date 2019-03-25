e50925f98aba3d90b73dd60c2d5f9b488995ef0b
Hubert Lin <linhub15@gmail.com>
2019-03-25 16:42:33 -0600

    Setup Main Page

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,24 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Home from "./components/home.component";
 import Category from "./components/category-view.component";
+import BusinessDetail from "./components/business-view.component";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <header className="App-header">
-            <h2>Contributors</h2>
-            <p id="hubert">Hubert</p>
-            <p id="ian">Ian</p>
-          </header>
-
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/categories" component={Category} />
+            <Route exact path="/business" component={BusinessDetail} />
           </Switch>
+
+          <footer className="app-footer">
+            <h2>Contributors</h2>
+            <p id="hubert">Hubert</p>
+            <p id="ian">Ian</p>
+          </footer>
         </div>
       </Router>
     );
